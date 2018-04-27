e5a44eadacf10d44e5763a8b010b843016f294dd
teamlead11 <automationlead11@gmail.com>
2018-04-27 08:49:12 +0530

    added logic for test case1 and test case2

--- src/TestCase2.java
+++ src/TestCase2.java
@@ -1,9 +1,12 @@
-
 public class TestCase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("added test case two");
+		System.out.println("added test case two");
 	}
 
+	public void multiplication(int a, int b) {
+		int c = a * b;
+		System.out.println(c);
+	}
 }
